23084c29fd334997b667a37d981725d93c2306f7
kosmos <kosmos.by@gmail.com>
2018-02-26 17:36:58 +0300

    added link on all magasins into sidebar

--- wa-apps/magasins/lib/actions/backend/magasinsBackendSidebar.action.php
+++ wa-apps/magasins/lib/actions/backend/magasinsBackendSidebar.action.php
@@ -4,8 +4,25 @@ class magasinsBackendSidebarAction extends waViewAction
 {
     public function execute()
     {
+
+        $request = waRequest::get();
+        $action = waRequest::get('action');
+
+        if (!$action) {
+            $action = waRequest::get('module');
+        }
+
+        $view_all_magasins = waRequest::get('all', null) !== null || empty($request);
+        $magasins_model = new magasinsMagasinModel();
+
+        $magasins_count = $magasins_model->countAll();
+
         $event_params = array();
         $this->view->assign('backend_sidebar', wa()->event('backend_sidebar', $event_params, array('top_li')));
+
+        $this->view->assign('view_all_magasins', $view_all_magasins);
+        $this->view->assign('new_magasin', waRequest::get('action') == 'edit' && waRequest::get('id') == '');
+        $this->view->assign('magasins_count', $magasins_count);
     }
 }
 
