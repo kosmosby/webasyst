190e48830b0b448d2c9be577cbd13ecd61f03fb1
kosmos <kosmos.by@gmail.com>
2018-06-27 18:54:19 +0300

    added preview 10 elements from xml

---
+++ wa-apps/magasins/lib/actions/matchs/magasinsMatchsRead.controller.php
@@ -0,0 +1,55 @@
+<?php
+
+class magasinsMatchsReadController extends waController
+{
+
+    public $array = array();
+    public $records_array = array();
+    public $select = '';
+
+    public function execute()
+    {
+        $provider_id = waRequest::request('provider_id');
+
+        $model = new magasinsProviderModel();
+
+        $json = array();
+
+        $array = array();
+        if(isset($provider_id) && $provider_id) {
+
+
+            $result = $model->query("SELECT category_id,name,parentId FROM magasins_categories WHERE provider_id = ".$provider_id." LIMIT 10");
+            $categories = $result->fetchAll();
+
+
+            $result = $model->query("SELECT * FROM magasins_products WHERE provider_id = ".$provider_id." LIMIT 10");
+            $products = $result->fetchAll();
+
+            $array['categories'] = $categories;
+            $array['products'] = $products;
+
+
+//
+//            for($i=0;$i<count($categories);$i++) {
+//                $array['categories']=$categories;
+//                $array[$data[$i]['db_field']]['multiply']  = $data[$i]['multiply'];
+//                $array[$data[$i]['db_field']]['has_a_key']  = $data[$i]['has_a_key'];
+//            }
+
+            $json = json_encode($array);
+
+//            echo "<pre>";
+//            print_r($json); die;
+        }
+
+        echo $json;
+
+
+    }
+
+
+
+
+
+}
