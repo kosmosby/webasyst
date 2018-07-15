6aadf18579c9dca6f23df91886a6914ae4d46694
kosmos <kosmos.by@gmail.com>
2018-07-15 14:36:02 +0300

    added filter for products list

--- wa-apps/magasins/lib/actions/products/magasinsProduct.action.php
+++ wa-apps/magasins/lib/actions/products/magasinsProduct.action.php
@@ -7,33 +7,58 @@ class magasinsProductAction extends waViewAction
 
 
         $search =  waRequest::post('search');
+        $filter_provider =  waRequest::post('filter_provider');
 
         $model = new magasinsProductModel();
 
         $this->setLayout( new magasinsDefaultLayout());
 
+        $provider_model = new magasinsProviderModel();
+
+        $providers = $provider_model->select('*')
+            ->where('published > 0')
+            //->where('id NOT IN (SELECT provider_id FROM magasins_setupmagasin WHERE magasin_id = '.$magasin_id.')')
+            ->order('name ASC')
+            ->fetchAll();
+
+
+        $sql_string = "SELECT a.*,b.name as category_name, c.name as provider FROM magasins_products as a, magasins_categories as b, magasins_provider as c  ";
+        $sql_string .= "WHERE a.categoryId = b.category_id AND c.id = a.provider_id ";
         if($search) {
-//            $records = $model->select('*')
-//                ->where('name LIKE \'%'.$search.'%\' OR url LIKE \'%'.$search.'%\'')
+            $sql_string .= " AND (a.name LIKE '%" . $search . "%' OR a.description LIKE '%" . $search . "%') ";
+        }
+        if($filter_provider) {
+            $sql_string .= " AND c.id = ".$filter_provider." ";
+        }
+        $sql_string .= " ORDER BY a.id ASC";
+        $sql = $model->query($sql_string);
+        $records = $sql->fetchAll();
+
+//        if($search) {
+////            $records = $model->select('*')
+////                ->where('name LIKE \'%'.$search.'%\' OR url LIKE \'%'.$search.'%\'')
+////
+////                ->order('id DESC')
+////                ->fetchAll();
 //
-//                ->order('id DESC')
-//                ->fetchAll();
-
-            $sql = $model->query("SELECT a.*,b.name as category_name FROM magasins_products as a, magasins_categories as b WHERE (a.name LIKE '%".$search."%' OR a.description LIKE '%".$search."%') AND a.categoryId = b.category_id ORDER BY a.id ASC");
-            $records = $sql->fetchAll();
-
-
-        }
-        else {
-//            $records = $model->order('id DESC')->fetchAll();
-
-            $sql = $model->query("SELECT a.*,b.name as category_name FROM magasins_products as a, magasins_categories as b where a.categoryId = b.category_id GROUP BY a.id ORDER BY a.id ASC");
-            $records = $sql->fetchAll();
-        }
+//            $sql = $model->query("SELECT a.*,b.name as category_name, c.name as provider FROM magasins_products as a, magasins_categories as b, magasins_provider as c WHERE (a.name LIKE '%".$search."%' OR a.description LIKE '%".$search."%') AND a.categoryId = b.category_id AND c.id = a.provider_id ORDER BY a.id ASC");
+//            $records = $sql->fetchAll();
+//
+//
+//        }
+//
+//        else {
+////            $records = $model->order('id DESC')->fetchAll();
+//
+//            $sql = $model->query("SELECT a.*,b.name as category_name, c.name as provider FROM magasins_products as a, magasins_categories as b, magasins_provider as c where a.categoryId = b.category_id AND c.id = a.provider_id GROUP BY a.id ORDER BY a.id ASC");
+//            $records = $sql->fetchAll();
+//        }
 
 //        echo "<pre>";
 //        print_r($records); die;
 
+        $this->view->assign('filter_provider', $filter_provider);
+        $this->view->assign('providers', $providers);
         $this->view->assign('records', $records);
         $this->view->assign('search', $search);
     }
