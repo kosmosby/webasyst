266ca6d2d15f5281af38981ea23a6ee9ac3191c6
kosmos <kosmos.by@gmail.com>
2018-10-22 22:20:14 +0300

    added setting for rel

--- wa-apps/magasins/lib/actions/setupmagasins/magasinsSetupmagasinSavesettings.controller.php
+++ wa-apps/magasins/lib/actions/setupmagasins/magasinsSetupmagasinSavesettings.controller.php
@@ -4,14 +4,11 @@ class magasinsSetupmagasinSavesettingsController extends waController
 {
     public function execute()
     {
-        $model = new magasinsSetupmagasinModel();
-        if (waRequest::method() == 'post') {
-            $magasin_id = waRequest::request('magasin_id');
-            $rel = waRequest::request('rel');
-
-            $model->deleteByField('magasin_id',$magasin_id);
-            $model->insert(array('rel'=>$rel,'magasin_id'=>$magasin_id));
-        }
+        $model = new magasinsSettingsModel();
+        $magasin_id = waRequest::request('magasin_id');
+        $rel = waRequest::request('rel');
+        $model->query("DELETE FROM magasins_magasin_settings WHERE magasin_id=".$magasin_id);
+        $model->query("INSERT INTO magasins_magasin_settings (magasin_id,rel) VALUES (".$magasin_id.", ".$rel.")");
         exit();
     }
 }
